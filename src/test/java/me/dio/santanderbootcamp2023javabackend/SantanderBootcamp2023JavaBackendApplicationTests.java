5e87dcdfe39bca30a1c33d0d3d33eba85fa88342
viniciusbdzn <viniciusbdzn@gmail.com>
2023-10-21 12:46:01 -0300

    Criando repositório do projeto RESTful API para a Santander Bootcamp 2023 - Java BackEnd

---
+++ src/test/java/me/dio/santanderbootcamp2023javabackend/SantanderBootcamp2023JavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderbootcamp2023javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcamp2023JavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
